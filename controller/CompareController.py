7ebbcbd805988e397e25e7077c911cc46f80c12c
tiboth <37446840+tiboth@users.noreply.github.com>
2019-06-02 13:26:11 +0300

    solved some problems

--- controller/CompareController.py
+++ controller/CompareController.py
@@ -13,11 +13,18 @@ class CompareController(Resource):
         self.main_service = service
 
     def post(self):
-        if request.headers['Content-Type'] == 'application/json':
+        if request.headers['Content-Type'] == 'application/json;charset=UTF-8':
             print("Request received")
             request_content = request.get_json()
-            return main_service.is_same_announcement(request_content['img_list1'], request_content['img_list2'],
+            print('img_list1: ' + ", ".join(request_content['img_list1']))
+            print('img_list2: ' + ", ".join(request_content['img_list2']))
+            print('description1: ' + request_content['description1'])
+            print('description2: ' + request_content['description2'])
+            result = main_service.is_same_announcement(request_content['img_list1'], request_content['img_list2'],
                                                      request_content['description1'], request_content['description2'])
+            print('result: ' + str(result))
+            print("=============================================================================")
+            return result
         else:
             return "Unsupported Media Type!"
 
--- service/MainService.py
+++ service/MainService.py
@@ -16,12 +16,12 @@ class MainService:
 
         similar_images = 0
 
-        gen = (img1 for img1 in img_list1 if similar_images < minim_similar_images)
-
-        for img1 in gen:
+        for img1 in img_list1:
             for img2 in img_list2:
                 if self.image_service.compare_images(img1, img2):
                     similar_images = similar_images + 1
+            if similar_images == 0:
+                break
 
         if similar_images < minim_similar_images:
             return False
@@ -35,17 +35,12 @@ class MainService:
             if value is not None:
                 if value == description_info2[key]:
                     same_info = same_info + 1
-        if same_info / len(description_info1.keys()) >= 0.75:
+        if same_info / len(description_info1.keys()) >= 0.50:
             return True
         else:
             return False
 
     def is_same_announcement(self, img_list1, img_list2, description1, description2):
-        print("First image list: " + ', '.join(str(elem) for elem in img_list1))
-        print("Second image list: " + ', '.join(str(elem) for elem in img_list2))
-        print("Description1: " + description1)
-        print("Description2: " + description2)
-
         description_info1 = self.description_service.get_description_info(description1)
         description_info2 = self.description_service.get_description_info(description2)
 
